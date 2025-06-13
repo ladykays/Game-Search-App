7f287aec9f96657b5ae0023468af39d118fdaea9
Kerrion Siokwu <ladykerrion@sky.com>
2025-06-13 22:15:15 +0100

    style(top-rated-games): adjust layout and styling - Adjust spacing and typography consistency - Adjust layout to only display 3 cards per row on large screens

--- index.js
+++ index.js
@@ -20,10 +20,11 @@ app.get("/", async(req, res) => {
   try {
     //console.log("API Key:", RAWG_API_KEY);
     //console.log("Request URL:", `${BASEURL}/games?key=${RAWG_API_KEY}&ordering=-rating&page=1`);
-    const response = await axios.get(`${BASEURL}/games?key=${RAWG_API_KEY}&ordering=-rating`);
-    const genresResponse = await axios.get(`${BASEURL}/genres?key=${RAWG_API_KEY}`)
+
+    const response = await axios.get(`${BASEURL}/games?key=${RAWG_API_KEY}&ordering=-rating&`);
+    //const genresResponse = await axios.get(`${BASEURL}/genres?key=${RAWG_API_KEY}`)
     const topRated = response.data.results;
-    console.log("Top Rated: ", topRated.map(game => game.name));
+    //console.log("Top Rated: ", topRated.map(game => game.name));
     res.render("index.ejs", { topRated: topRated });
 
   } catch (error) {
