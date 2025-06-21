0a17cef068f5f60eb293e0a930bfea9a1e96f2a8
Kerrion Siokwu <ladykerrion@sky.com>
2025-06-21 05:00:47 +0100

    feat: Implement game genres section with dynamic API data
    Fetch and display genres from API
    Conditionally render genre-specific slogans on the genre section of the  homepage
    Add genre route (/search) showing game count per category
    (Incomplete) Genre details page (WIP)

--- src/routes/gameRoutes.js
+++ src/routes/gameRoutes.js
@@ -3,6 +3,8 @@ import {
   getHomePage, 
   getResultsPage,
   getGamePage,
+  getGenresPage,
+  getSearchPage,
 } from "../controllers/gameController.js";
 
 const router = express.Router();
@@ -10,5 +12,7 @@ const router = express.Router();
 router.get("/", getHomePage);
 router.get("/results", getResultsPage);
 router.get("/game/:id", getGamePage);
+router.get("/genres", getGenresPage);
+router.get("/search", getSearchPage)
 
 export  default router;
